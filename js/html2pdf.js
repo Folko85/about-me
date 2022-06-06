5b772b3046c902f6e32e0c16cc3391365f95f21a
Gutorov <Folko85@yandex.ru>
2022-06-06 15:58:22 +0300

    Изменил текстовку и чуток параметры.

--- js/html2pdf.js
+++ js/html2pdf.js
@@ -1,11 +1,12 @@
 const page_break = {mode: '', before: '.before', after: '.after', avoid: '.avoid'};
-
 function generatePDF() {
     const element = document.getElementById('invoice');
     html2pdf()
         .from(element)
         .set({
-            filename: 'GutorovmnJava.pdf',
+            filename: 'GutorovMNJava.pdf',
+            headless: true,
+            slowMo: 150,
             pagebreak: page_break,
             jsPDF: {orientation: 'portrait', unit: 'in', format: 'letter', compressPDF: true}
         })
